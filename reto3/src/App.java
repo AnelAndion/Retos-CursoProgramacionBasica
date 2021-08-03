752cbfa722514c36a354ee5a20d1ec8b280c4420
Anel <aandion@uninorte.edu.co>
2021-08-03 18:44:06 -0500

    Version 3.0.0 - Agrege primer metodo en la clase GrandingSystem

--- reto3/src/App.java
+++ reto3/src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) {
         SchoolGradingSystem statistics = new SchoolGradingSystem();
         statistics.loadData();
+        //output 1
+        System.out.println(statistics.stat1());
     }
 }
--- reto3/src/GradingSystem.java
+++ reto3/src/GradingSystem.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class GradingSystem {
    protected Scanner read = new Scanner(System.in);
    protected  int n;
    protected double[][] data;
 
+   // method 1 -- stat1
+   public double stat1(){
+      DecimalFormat f = new DecimalFormat("0.00");
+      double summation = 0.0, main;
+      // summation
+      for (int i = 0; i < n; i++){
+         summation += data[i][3];
+      }
+      // main
+      main = summation/n;
+      return Double.parseDouble(f.format(main)); // output
+   }
 }
